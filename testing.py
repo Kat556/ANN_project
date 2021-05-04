222be81383c692255d11195e9af5d63da7240c32
Jaeseong Kim <60629437+JaesKim@users.noreply.github.com>
2021-05-04 11:51:04 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -42,7 +42,8 @@ test_dataset = test_dataset.padded_batch(BATCH_SIZE)
 
 model = tf.keras.Sequential([
     tf.keras.layers.Embedding(encoder.vocab_size, 64),
-    tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(64, activation='relu'),
     tf.keras.layers.Dense(1)
 ])
